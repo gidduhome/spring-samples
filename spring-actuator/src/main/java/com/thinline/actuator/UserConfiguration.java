1d5a632a86b5fad7bea437cbf613002671ad8e8a
gidduhome <VGiddaluri@PathologyPartners.intranet>
2016-07-08 13:54:01 -0500

    First spring-actuator commit
    This is a simple spring-actuator project. The purpose of this is see
    what comes with basic spring-actuator. It also overridden server port
    and management port.

---
+++ spring-actuator/src/main/java/com/thinline/actuator/UserConfiguration.java
@@ -0,0 +1,13 @@
+package com.thinline.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserConfiguration.class, args);
+    }
+
+}

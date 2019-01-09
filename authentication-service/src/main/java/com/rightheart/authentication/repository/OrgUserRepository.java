69677b289a38604c0c3bcf8f5bea97a6e51e741f
Venkat <gidduhome@gmail.com>
2019-01-09 10:54:14 -0600

    Committing authentication service

---
+++ authentication-service/src/main/java/com/rightheart/authentication/repository/OrgUserRepository.java
@@ -0,0 +1,11 @@
+package com.rightheart.authentication.repository;
+
+
+import com.rightheart.authentication.model.UserOrganization;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrgUserRepository extends CrudRepository<UserOrganization,String>  {
+    public UserOrganization findByUserName(String userName);
+}
